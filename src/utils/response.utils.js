52b05e0080a1bd3890f16798515090ec12a4244b
SricharanSS <sricharan.senthilarasu+cdwcorp@cdw.com>
2025-09-26 19:14:18 +0530

    update response.utils

--- src/utils/response.utils.js
+++ src/utils/response.utils.js
@@ -1,7 +1,9 @@
-export const sendResponse = (res, data, message, code) => {
+const sendResponse = (res, data, message, code) => {
   res.status(code).json({
     code,
     message,
     data,
   })
 }
+
+module.exports = {sendResponse}
